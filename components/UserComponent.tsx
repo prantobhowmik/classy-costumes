1941a0893ba4f432e9c2abf16dcf15ca4c1b903a
prantobhowmik <bhowmikpranto@gmail.com>
2024-07-27 16:10:59 +0530

    fixed(pranto): errors

--- components/UserComponent.tsx
+++ components/UserComponent.tsx
@@ -4,10 +4,10 @@ import {User} from "@nextui-org/react";
 export default function UserComponent() {
   return (
     <User
-        className="flex flex-col items-center justify-center font-bold pt-2"
+        className="flex flex-col items-center justify-center -gap-5 font-bold pt-2"
       avatarProps={{
         src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
       }}
-    />
+     name="Ms Lotifa"/>
   );
 }
