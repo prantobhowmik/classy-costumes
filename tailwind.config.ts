1cb4260d6ca77d8ae7083cd520291ed5858a88a7
prantobhowmik <bhowmikpranto@gmail.com>
2024-07-29 00:06:33 +0530

    tes config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,25 @@
-import type { Config } from "tailwindcss";
-import {any} from "prop-types";
-const {nextui} = require("@nextui-org/react");
+import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/react'; // Import directly
 
-
-const config: {
-  plugins: ReturnType<Plugin>[];
-  theme: { extend: { backgroundImage: { "gradient-conic": string; "gradient-radial": string } } };
-  darkMode: string;
-  content: string[]
-} = {
+const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-   darkMode: "class",
-  plugins: [nextui()]
+  darkMode: 'class',
+  plugins: [
+    nextui(), // Ensure this is correctly applied
+  ],
 };
+
 export default config;
